313ce170a8ca0e325a6acefd9a3e3e3aa290b2bf
Nathaniel Coleman <kyloren@turtleshelldesigns.com>
2024-01-17 16:44:39 +0000

    add GET values

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the reviews index.")
+    name = request.GET.get("name") or "World"
+    return HttpResponse(f"Hello, {name}. You're at the reviews index.")
